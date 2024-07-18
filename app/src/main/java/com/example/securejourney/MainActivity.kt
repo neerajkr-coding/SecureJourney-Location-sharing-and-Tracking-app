25ccba676fa950b93c8a868af17d424ddf244760
neerajkr-coding <neerajkr600@gmail.com>
2024-07-18 17:47:24 +0530

    Initial commit Bottom Navigation Bar and Splash Screen

---
+++ app/src/main/java/com/example/securejourney/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.securejourney
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.animation.AnimationUtils
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
